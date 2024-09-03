ac3650c211e535e75f195cc684e51510f3cf20b8
Sandhya Rajwanshi <47480884+SandhyaR1007@users.noreply.github.com>
2024-09-03 23:27:24 +0530

    update: comments

--- src/components/Comments.tsx
+++ src/components/Comments.tsx
@@ -4,6 +4,7 @@ export const Comments = ({ comments }: CommentsProps) => {
   return (
     <div>
       {comments.map((ele: Comment) => {
+        if (ele.replies.length) return <Comments comments={ele.replies} />;
         return <div>{ele.text}</div>;
       })}
     </div>
